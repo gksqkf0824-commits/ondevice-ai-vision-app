f5cb353209643cb24037ee5c2b18d31439f4a203
soyun <gksqkf0824@gmail.com>
2026-05-29 00:02:10 +0900

    fix: resolve server connection and signup issues for Android app

--- mobile/app/src/main/java/com/example/ondevice/network/ApiService.kt
+++ mobile/app/src/main/java/com/example/ondevice/network/ApiService.kt
@@ -34,7 +34,7 @@ interface ApiService {
 }
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "http://192.168.45.70:8080/"
 
     val instance: ApiService by lazy {
         Retrofit.Builder()
